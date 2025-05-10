51b86a6583a43e1783b59fa9274f7c8c7577ac71
TheRealYogurt <lirian_dano@hotmail.com>
2025-05-10 14:28:03 +0100

    Added the onsite energies and hoppings beween W and Te

--- Tungsten_Ditelluride/T_prime_phase.py
+++ Tungsten_Ditelluride/T_prime_phase.py
@@ -5,30 +5,36 @@ from math import sqrt
 pb.pltutils.use_style()
 
 def monolayer_1T_WTe2():
-    a = 6.314769; b = 3.492485
-    a1=[a, 0]; a2=[0, b]; t = 1
-    lat = pb.Lattice( a1, a2) #a3 = [0, 0, c]) # Define rectangular lattice
+    a = 6.314769; b = 3.492485; a1=[a, 0]; a2=[0, b] # lattice vectors
+
+    # Onsite energies p -> Te , d -> W, lattice hopping energies 
+    mu_d = 0.22665; mu_p = -1.66528; t = [0.95946, -0.25719, 0.37810, -0.15573]  
+
+    lat = pb.Lattice( a1, a2) # Rectangular 2D lattice
 
     lat.add_sublattices(
-
-        ('W1', [0.821873 * a, 0.750000 * b]), ('W2', [0.178127 * a, 0.250000 * b]),
-        ('Te1',[0.081802 * a, 0.750000 * b]), ('Te2',[0.572013 * a, 0.250000 * b]),
-        ('Te3',[0.427987 * a, 0.750000 * b]), ('Te4',[0.918198 * a, 0.250000 * b])
+        #main cell
+        ('W1', [0.821873 * a, 0.750000 * b], mu_d), ('W2', [0.178127 * a, 0.250000 * b], mu_d), 
+        ('Te1',[0.081802 * a, 0.750000 * b], mu_p), ('Te2',[0.572013 * a, 0.250000 * b], mu_p),
+        ('Te3',[0.427987 * a, 0.750000 * b], mu_p), ('Te4',[0.918198 * a, 0.250000 * b], mu_p)
     )
 
     lat.add_hoppings(
         #main cell hoppings 
-        ([0, 0], 'W1', 'Te3', t), ([0, 0], 'W1', 'Te2', t),
-        ([0, 0], 'W2', 'Te3', t), ([0, 0], 'W2', 'Te2', t),
-        ([0, 0], 'W2', 'Te1', t), ([0, 0], 'W1', 'Te4', t),
+        ([0, 0], 'W1', 'Te3', t[1]), 
+        ([0, 0], 'W1', 'Te2', t[0]), 
+        ([0, 0], 'W2', 'Te3', t[0]), 
+        ([0, 0], 'W2', 'Te2', t[1]),
+        ([0, 0], 'W2', 'Te1', t[2]), 
+        ([0, 0], 'W1', 'Te4', t[3]),
 
         #between neighbouring cells
-        ([0,1],'Te1','W2', t),
-        ([0,1],'Te3','W2', t),
-        ([0,1],'W1','Te2', t),
-        ([1,0],'W1','Te1', t),
-        ([1,0],'Te4','W2', t),
-        ([0,-1],'Te4','W1', t)
+        ([0,1],'Te1','W2', t[2]),
+        ([0,1],'Te3','W2', t[3]),
+        ([0,1],'W1','Te2', t[0]),
+        ([1,0],'W1','Te1', t[3]),
+        ([1,0],'Te4','W2', t[3]),
+        ([0,-1],'Te4','W1', t[2])
         )
     return lat
 
