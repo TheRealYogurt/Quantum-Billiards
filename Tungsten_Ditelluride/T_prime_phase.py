c4db18825452ab1ff2b9d2b4c43fbe37561347e5
TheRealYogurt <lirian_dano@hotmail.com>
2025-07-01 10:40:33 +0100

    working model of 1T'-WTe2

--- Tungsten_Ditelluride/T_prime_phase.py
+++ Tungsten_Ditelluride/T_prime_phase.py
@@ -1,45 +1,229 @@
-import pybinding as pb
+import pybinding as pb, numpy as np
 import matplotlib.pyplot as plt
 from math import sqrt
 
 pb.pltutils.use_style()
 
-def monolayer_1T_WTe2():
-    a = 6.314769; b = 3.492485; a1=[a, 0]; a2=[0, b] # lattice vectors
+def monolayer_1T_WTe2_SOC():
+    # lattice vectors
+    a = 3.477; b = 6.249; a1=[a, 0]; a2=[0, b] 
 
-    # Onsite energies p -> Te , d -> W, lattice hopping energies 
-    mu_d = 0.22665; mu_p = -1.66528; t = [0.95946, -0.25719, 0.37810, -0.15573]  
+    #nsite energies d -> W, p -> Te
+    mu_d = 0.74; mu_d_spinor = mu_d * np.identity(2)
+    mu_p = -1.75; mu_p_spinor = mu_p * np.identity(2)
+    
+    # Hopping energies in eV: 
+    t_px = 1.13; t_dx = -0.41; t_pAB = 0.40; t_dAB = 0.51 
+    t_0AB = 0.39; t_0ABx = 0.29; t_0x = 0.14; t_py = 0.13   
 
+    # Spin Orbit Energies in eV:
+    lambda_0AB_y = 0.011; lambda_0_y = 0.051; lambda_0_z = 0.012
+    lambda_0_y_p = 0.050; lambda_0_z_p = 0.012; lambda_px_y = -0.040 
+    lambda_pxz = -0.010; lambda_dx_y = -0.031; lambda_dx_z = -0.008 
+
+    # Pauli Matricies: 
+    complex_matrix = np.full((2, 2), 1j)
+
+    sigma_0 = np.array([[1, 0], [0, 1]]); sigma_0_i = sigma_0
+    sigma_x = np.array([[0, 1], [1, 0]]); sigma_x_i = np.multiply(sigma_x, complex_matrix)
+    sigma_y = np.array([[0, -1j], [1j, 0]]); sigma_y_i = np.multiply(sigma_y, complex_matrix)
+    sigma_z = np.array([[1, 0], [0, -1]]); sigma_z_i = np.multiply(sigma_z, complex_matrix)
+  
     lat = pb.Lattice( a1, a2) # Rectangular 2D lattice
 
     lat.add_sublattices(
-        #main cell
-        ('W1', [0.821873 * a, 0.750000 * b], mu_d), ('W2', [0.178127 * a, 0.250000 * b], mu_d), 
-        ('Te1',[0.081802 * a, 0.750000 * b], mu_p), ('Te2',[0.572013 * a, 0.250000 * b], mu_p),
-        ('Te3',[0.427987 * a, 0.750000 * b], mu_p), ('Te4',[0.918198 * a, 0.250000 * b], mu_p)
+        # Main cell: 
+        ('W1', [ -0.25 * a, 0.32* b], mu_d_spinor), # Sublattice A 
+        ('Te1',[-0.25 * a, -0.07 * b], mu_p_spinor), # Sublattice A
+        ('W2', [ 0.25 * a, -0.32 * b], mu_d_spinor), # Sublattice B 
+        ('Te2',[ 0.25 * a, 0.07 * b], mu_p_spinor) # Sublattice B 
     )
+    
+    #lat.add_sublattices(('Te1_p',[-0.25 * a, -0.07 * b], mu_p_spinor)), # Sublattice A)
+
+    # Function to calculate the hopping parameter
+    def hopping_parameter(t, lambda_x, lambda_y, lambda_z): 
+        parameter =  np.add(np.add(np.multiply(t,sigma_0_i), np.multiply(sigma_x_i, lambda_x)), 
+                            np.add(np.multiply(sigma_y_i, lambda_y), np.multiply(sigma_z_i, lambda_z)))
+        return parameter
 
     lat.add_hoppings(
-        #main cell hoppings 
-        ([0, 0], 'W1', 'Te3', t[1]), 
-        ([0, 0], 'W1', 'Te2', t[0]), 
-        ([0, 0], 'W2', 'Te3', t[0]), 
-        ([0, 0], 'W2', 'Te2', t[1]),
-        ([0, 0], 'W2', 'Te1', t[2]), 
-        ([0, 0], 'W1', 'Te4', t[3]),
+        # Main Cell Hoppings: Between atomms in the cell
+        ([0, 0], 'Te1', 'W1', hopping_parameter(t = 0, lambda_x = 0, lambda_y = -lambda_0_y, lambda_z = -lambda_0_z)), 
+        ([0, 0], 'Te2', 'W1', hopping_parameter( t = t_0AB, lambda_x = 0, lambda_y = -lambda_0AB_y, lambda_z = 0)), 
 
-        #between neighbouring cells
-        ([0,1],'Te1','W2', t[2]),
-        ([0,1],'Te3','W2', t[3]),
-        ([0,1],'W1','Te2', t[0]),
-        ([1,0],'W1','Te1', t[3]),
-        ([1,0],'Te4','W2', t[3]),
-        ([0,-1],'Te4','W1', t[2])
-        )
+        ([0, 0], 'Te2', 'Te1', hopping_parameter(t = t_pAB, lambda_x = 0, lambda_y = 0, lambda_z = 0)),
+        ([0, 0], 'W2', 'Te1', hopping_parameter(t = -t_0AB, lambda_x = 0, lambda_y = -lambda_0AB_y, lambda_z = 0)), 
+
+        ([0, 0], 'Te2', 'W2', hopping_parameter(t = 0, lambda_x = 0, lambda_y = lambda_0_y, lambda_z = lambda_0_z)), 
+
+        # Between Cells Hoppings: From inside the cell to the right (outside the cell)   
+        ([1, 0], 'W1', 'W1', hopping_parameter(t = t_dx, lambda_x = 0, lambda_y = -lambda_dx_y, lambda_z = -lambda_dx_z)),
+        ([1, 0], 'W2', 'W2', hopping_parameter(t = t_dx, lambda_x = 0, lambda_y = lambda_dx_y, lambda_z = lambda_dx_z)), 
+
+        ([1, 0], 'Te2', 'Te2', hopping_parameter(t = t_px, lambda_x = 0, lambda_y = lambda_px_y, lambda_z = lambda_pxz)), 
+        ([1, 0], 'Te1', 'Te1', hopping_parameter(t = t_px, lambda_x = 0, lambda_y = -lambda_px_y, lambda_z = -lambda_pxz )),
+
+        ([1, 0], 'Te2', 'Te1', hopping_parameter(t = t_pAB, lambda_x = 0, lambda_y = 0, lambda_z = 0 )),
+        
+        ([1, 0], 'Te2', 'W1', hopping_parameter(t = -t_0AB, lambda_x = 0, lambda_y = -lambda_0AB_y, lambda_z = 0)),
+        
+        ([1, 0], 'W2', 'Te1', hopping_parameter(t = t_0AB , lambda_x = 0, lambda_y = -lambda_0AB_y, lambda_z = 0)),
+
+        ([1, 0], 'W1', 'Te2', hopping_parameter(t = t_0ABx, lambda_x = 0, lambda_y = 0, lambda_z = 0)),  
+        ([1, 0], 'W1', 'Te1', hopping_parameter(t = t_0x, lambda_x = 0, lambda_y = 0, lambda_z = 0)),  
+
+        ([1, 0], 'Te1', 'W2', hopping_parameter(t = -t_0ABx, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+        
+        ([1, 0], 'W2', 'Te2', hopping_parameter(t = t_0x, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+        
+        # Between Cells Hoppings: From inside the cell to the bottom (outside the cell)
+        ([0, -1], 'Te1', 'W1', hopping_parameter(t = 0, lambda_x = 0, lambda_y = -lambda_0_y_p, lambda_z = -lambda_0_z_p)),
+        
+        ([0, -1], 'W2', 'W1', hopping_parameter(t = t_dAB, lambda_x = 0, lambda_y = 0, lambda_z = 0)),
+        ([1, -1], 'W2', 'W1', hopping_parameter(t = t_dAB, lambda_x = 0, lambda_y = 0, lambda_z = 0)),
+        
+        ([0, -1], 'Te2', 'Te2', hopping_parameter(t = t_py, lambda_x = 0, lambda_y = 0, lambda_z = 0)),
+
+        # Between Cells Hoppings: From inside the cell to the top (outside the cell)
+        ([0, 1], 'Te2', 'W2', hopping_parameter(t = 0, lambda_x = 0, lambda_y = lambda_0_y_p, lambda_z = lambda_0_z_p)), 
+        
+        ([0, 1], 'Te1', 'Te1', hopping_parameter(t = t_py, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+                
+        # Between Cells Hoppings: From inside the cell to the left (outside the cell)
+        ([-1, 0], 'W2', 'Te2', hopping_parameter(t = -t_0x, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+
+        # Between Cells Hoppings: From outside the cell to the inside(from the right)
+        ([-1, 0], 'W1', 'Te1', hopping_parameter(t = -t_0x, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+
+        # Between Cells Hoppings: Next cell over hoppoings(tothe left)
+        ([-2, 0], 'W1', 'Te2', hopping_parameter(t = -t_0ABx, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+        ([-2, 0], 'Te1', 'W2', hopping_parameter(t = t_0ABx, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+    )
+
     return lat
 
-lattice = monolayer_1T_WTe2(); lattice.plot()
 
-scale = 3; shape = pb.circle(radius = 3 * scale)
-model = pb.Model(lattice,shape); #model.plot()
+def monolayer_1T_WTe2_basic():
+    # lattice vectors
+    a = 3.477; b = 6.249; a1=[a, 0]; a2=[0, b] 
+
+    #nsite energies d -> W, p -> Te
+    mu_d = 0.74; mu_d_spinor = mu_d * np.identity(2)
+    mu_p = -1.75; mu_p_spinor = mu_p * np.identity(2)
+    
+    # Hopping energies in eV: 
+    t_px = 1.13; t_dx = -0.41; t_pAB = 0.40; t_dAB = 0.51 
+    t_0AB = 0.39; t_0ABx = 0.29; t_0x = 0.14; t_py = 0.13   
+
+    # Spin Orbit Energies in eV:
+    lambda_0AB_y = 0.011; lambda_0_y = 0.051; lambda_0_z = 0.012
+    lambda_0_y_p = 0.050; lambda_0_z_p = 0.012; lambda_px_y = -0.040 
+    lambda_pxz = -0.010; lambda_dx_y = -0.031; lambda_dx_z = -0.008 
+
+    # Pauli Matricies: 
+    complex_matrix = np.full((2, 2), 1j)
+
+    sigma_0 = np.array([[1, 0], [0, 1]]); sigma_0_i = sigma_0
+    sigma_x = np.array([[0, 1], [1, 0]]); sigma_x_i = np.multiply(sigma_x, complex_matrix)
+    sigma_y = np.array([[0, -1j], [1j, 0]]); sigma_y_i = np.multiply(sigma_y, complex_matrix)
+    sigma_z = np.array([[1, 0], [0, -1]]); sigma_z_i = np.multiply(sigma_z, complex_matrix)
+  
+    lat = pb.Lattice( a1, a2) # Rectangular 2D lattice
+
+    lat.add_sublattices(
+        # Main cell: 
+        ('W1', [ -0.25 * a, 0.32* b], mu_d_spinor), # Sublattice A 
+        ('Te1',[-0.25 * a, -0.07 * b], mu_p_spinor), # Sublattice A
+        ('W2', [ 0.25 * a, -0.32 * b], mu_d_spinor), # Sublattice B 
+        ('Te2',[ 0.25 * a, 0.07 * b], mu_p_spinor) # Sublattice B 
+    )
+    
+    #lat.add_sublattices(('Te1_p',[-0.25 * a, -0.07 * b], mu_p_spinor)), # Sublattice A)
+
+    # Function to calculate the hopping parameter
+    def hopping_parameter(t, lambda_x, lambda_y, lambda_z): 
+        parameter =  np.add(np.add(np.multiply(t,sigma_0_i), np.multiply(sigma_x_i, lambda_x)), 
+                            np.add(np.multiply(sigma_y_i, lambda_y), np.multiply(sigma_z_i, lambda_z)))
+        return parameter
+
+    lat.add_hoppings(
+        # Main Cell Hoppings: Between atomms in the cell
+        ([0, 0], 'Te1', 'W1', hopping_parameter(t = 0, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+        ([0, 0], 'Te2', 'W1', hopping_parameter( t = t_0AB, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+
+        ([0, 0], 'Te2', 'Te1', hopping_parameter(t = t_pAB, lambda_x = 0, lambda_y = 0, lambda_z = 0)),
+        ([0, 0], 'W2', 'Te1', hopping_parameter(t = -t_0AB, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+
+        ([0, 0], 'Te2', 'W2', hopping_parameter(t = 0, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+
+        # Between Cells Hoppings: From inside the cell to the right (outside the cell)   
+        ([1, 0], 'W1', 'W1', hopping_parameter(t = t_dx, lambda_x = 0, lambda_y = 0, lambda_z = 0)),
+        ([1, 0], 'W2', 'W2', hopping_parameter(t = t_dx, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+
+        ([1, 0], 'Te2', 'Te2', hopping_parameter(t = t_px, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+        ([1, 0], 'Te1', 'Te1', hopping_parameter(t = t_px, lambda_x = 0, lambda_y = 0, lambda_z = 0 )),
+
+        ([1, 0], 'Te2', 'Te1', hopping_parameter(t = t_pAB, lambda_x = 0, lambda_y = 0, lambda_z = 0 )),
+        
+        ([1, 0], 'Te2', 'W1', hopping_parameter(t = -t_0AB, lambda_x = 0, lambda_y = 0, lambda_z = 0)),
+        
+        ([1, 0], 'W2', 'Te1', hopping_parameter(t = t_0AB , lambda_x = 0, lambda_y = 0, lambda_z = 0)),
+
+        ([1, 0], 'W1', 'Te2', hopping_parameter(t = t_0ABx, lambda_x = 0, lambda_y = 0, lambda_z = 0)),  
+        ([1, 0], 'W1', 'Te1', hopping_parameter(t = t_0x, lambda_x = 0, lambda_y = 0, lambda_z = 0)),  
+
+        ([1, 0], 'Te1', 'W2', hopping_parameter(t = -t_0ABx, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+        
+        ([1, 0], 'W2', 'Te2', hopping_parameter(t = t_0x, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+        
+        # Between Cells Hoppings: From inside the cell to the bottom (outside the cell)
+        ([0, -1], 'Te1', 'W1', hopping_parameter(t = 0, lambda_x = 0, lambda_y = 0, lambda_z = 0)),
+        
+        ([0, -1], 'W2', 'W1', hopping_parameter(t = t_dAB, lambda_x = 0, lambda_y = 0, lambda_z = 0)),
+        ([1, -1], 'W2', 'W1', hopping_parameter(t = t_dAB, lambda_x = 0, lambda_y = 0, lambda_z = 0)),
+        
+        ([0, -1], 'Te2', 'Te2', hopping_parameter(t = t_py, lambda_x = 0, lambda_y = 0, lambda_z = 0)),
+
+        # Between Cells Hoppings: From inside the cell to the top (outside the cell)
+        ([0, 1], 'Te2', 'W2', hopping_parameter(t = 0, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+        
+        ([0, 1], 'Te1', 'Te1', hopping_parameter(t = t_py, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+                
+        # Between Cells Hoppings: From inside the cell to the left (outside the cell)
+        ([-1, 0], 'W2', 'Te2', hopping_parameter(t = -t_0x, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+
+        # Between Cells Hoppings: From outside the cell to the inside(from the left)
+        ([-1, 0], 'W1', 'Te1', hopping_parameter(t = -t_0x, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+
+        # Between Cells Hoppings: Next cell over hoppoings(tothe left)
+        ([-2, 0], 'W1', 'Te2', hopping_parameter(t = -t_0ABx, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+        ([-2, 0], 'Te1', 'W2', hopping_parameter(t = t_0ABx, lambda_x = 0, lambda_y = 0, lambda_z = 0)), 
+    )
+
+    return lat
+
+
+# High symmetry points for a rectangular lattice: 
+a = 3.477; b = 6.249; a1=[a, 0]; a2=[0, b] 
+Gamma = np.array([0, 0])    # Gamma Point
+X = np.array([np.pi/a, 0])  # X Point
+Y = np.array([0, np.pi/b])  # Y Point
+M = np.array([np.pi/a, np.pi/b])  # M Point
+
+plt.figure()
+model_soc = pb.Model(monolayer_1T_WTe2_SOC(), pb.translational_symmetry())
+solver_soc = pb.solver.lapack(model_soc)
+bands_soc = solver_soc.calc_bands(Gamma, X, Y, M)
+bands_soc.plot(point_labels=[r'$\Gamma$', 'X', 'Y', 'M'])
+plt.title("Band Structure: Spin Orbit Coupling")
+
+
+plt.figure()
+model_basic = pb.Model(monolayer_1T_WTe2_basic(), pb.translational_symmetry())
+solver_basic = pb.solver.lapack(model_basic)
+band_basic = solver_basic.calc_bands(Gamma, X, Y, M)
+band_basic.plot(point_labels=[r'$\Gamma$', 'X', 'Y', 'M'])
+plt.title("Band Structure: Basic")
+
 plt.show()
